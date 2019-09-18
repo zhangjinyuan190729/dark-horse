edcbecf77e526e1d826db21b0f5b6b39d430ba26
zhangjinyuan <2427938729@qq.com>
2019-09-18 21:15:14 +0800

    初始化完成

--- src/router.js
+++ src/router.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(Router)
-
+Vue.use(ElementUI)
 export default new Router({
   routes: [
     {
@@ -13,11 +14,11 @@ export default new Router({
     },
     {
       path: '/about',
-      name: 'about',
+      name: 'about'
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      // component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
