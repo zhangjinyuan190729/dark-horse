15925277f8b39214579877ddf634b069f8d9b1af
zhangjinyuan <2427938729@qq.com>
2019-09-23 11:34:26 +0800

    定制用户协议

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -6,10 +6,14 @@
     <!-- 表格 -->
     <el-table :data="list">
         <el-table-column width="600px"  label="标题" prop="title"></el-table-column>
-        <el-table-column align="center" label="评论状态" prop="comment_status"></el-table-column>
+                            <!-- 绑定回调函数 -->
+        <el-table-column :formatter="formatter" align="center" label="评论状态" prop="comment_status"></el-table-column>
         <el-table-column align="center" label="总评论数" prop="total_comment_count" ></el-table-column>
         <el-table-column align="center" label="粉丝评论数" prop="fans_comment_count" ></el-table-column>
-        <el-table-column align="center" label="操作"  ></el-table-column>
+        <el-table-column align="center" label="操作">
+          <el-button size="small" type="text">修改</el-button>
+          <el-button size="small" type="text">关闭评论</el-button>
+        </el-table-column>
     </el-table>
   </el-card>
 </template>
@@ -29,6 +33,9 @@ export default {
       }).then(result => {
         this.list = result.data.results
       })
+    },
+    formatter (row, column, cellvale, index) {
+      return cellvale ? '正常' : '关闭'
     }
   },
   created () {
